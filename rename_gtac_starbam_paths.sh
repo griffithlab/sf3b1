bdf361a3c42c39ecceadc2fb4b97e902d16aa3a2
Malachi Griffith <malachig@gmail.com>
2020-08-13 11:54:56 -0500

    add scripts for producing individual exon counts

---
+++ rename_gtac_starbam_paths.sh
@@ -0,0 +1,15 @@
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-1.GGAGTCC/i-1.GGAGTCC.genome_accepted_hits.bam /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/Control_Rep1.bam
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-2.TGGCATA/i-2.TGGCATA.genome_accepted_hits.bam /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/Control_Rep2.bam 
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-3.GTAAAAA/i-3.GTAAAAA.genome_accepted_hits.bam /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/Control_Rep3.bam 
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-4.CTGCAAT/i-4.CTGCAAT.genome_accepted_hits.bam /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/SF3B1_Rep1.bam 
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-5.AATTCAT/i-5.AATTCAT.genome_accepted_hits.bam /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/SF3B1_Rep2.bam 
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-6.AGGCGAA/i-6.AGGCGAA.genome_accepted_hits.bam /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/SF3B1_Rep3.bam 
+
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-1.GGAGTCC/i-1.GGAGTCC.genome_accepted_hits.bam.bai /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/Control_Rep1.bam.bai
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-2.TGGCATA/i-2.TGGCATA.genome_accepted_hits.bam.bai /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/Control_Rep2.bam.bai
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-3.GTAAAAA/i-3.GTAAAAA.genome_accepted_hits.bam.bai /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/Control_Rep3.bam.bai
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-4.CTGCAAT/i-4.CTGCAAT.genome_accepted_hits.bam.bai /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/SF3B1_Rep1.bam.bai 
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-5.AATTCAT/i-5.AATTCAT.genome_accepted_hits.bam.bai /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/SF3B1_Rep2.bam.bai 
+cp /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/gtac/Kommagani_2938_5/i-6.AGGCGAA/i-6.AGGCGAA.genome_accepted_hits.bam.bai /storage1/fs1/mgriffit/Active/kommagani_SplicingAnalysis/inputs/SF3B1_Rep3.bam.bai 
+
+
